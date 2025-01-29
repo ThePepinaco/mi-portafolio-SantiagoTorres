abfe3b2d9b2a123779c19973e42960a1f20a8f43
ThePepinaco <thepepinaco@gmail.com>
2025-01-28 19:04:52 -0500

    init

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component'; // Tu componente principal
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // Redirigir todas las rutas internas hacia la página principal
+  { path: '', component: AppComponent },
+  { path: '**', redirectTo: '', pathMatch: 'full' } // Redirigir cualquier ruta no definida a la principal
+];
+
